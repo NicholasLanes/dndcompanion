e9a5bb89808c5e188dce8dfc5763fa3dd05cc9d3
NicholasLanes <97725017+NicholasLanes@users.noreply.github.com>
2022-04-09 12:58:04 -0500

    Functioning View Full Char Sheet
    Utilizes conditional view to populate the detailed character view with all attributes applicable to the selected character.

---
+++ Models/CharacterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using dnd.Models.Characters;
+using dnd.Models.Session;
+using Microsoft.AspNetCore.Http;
+
+namespace dnd.Models
+{
+    public class CharacterViewModel
+    {
+        private Character character;
+
+        public Character Character 
+        { 
+            get => character;
+            set
+            {
+                character = value;
+            }
+        }
+    }
+}
